8e8dc486ec0732f8aeea2f76bee86b305f0c6ab6
Connor Hudson <cohu3291@colorado.edu>
2018-06-01 09:36:33 -0600

    Remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -53,18 +53,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -116,8 +104,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButtonLocation: 
         FloatingActionButtonLocation.centerDocked,
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: (){},
+        tooltip: 'New Post',
         child: new Icon(Icons.camera_alt),
         notchMargin: 4.0,
       ), // This trailing comma makes auto-formatting nicer for build methods.
