e2a79835e46f41bc1daa067915f157788ee4caaf
Connor Hudson <cohu3291@colorado.edu>
2018-06-04 22:22:43 -0400

    Fix text layout issues in ImageCaption, add CircleAvatar, make sections into functions

--- lib/components/FeedImage.dart
+++ lib/components/FeedImage.dart
@@ -8,44 +8,61 @@ class FeedImage extends StatefulWidget {
 class FeedImageState extends State<FeedImage> {
   @override
   Widget build(BuildContext context) {
-    Widget userHeader = Container(
-      padding: const EdgeInsets.all(24.0),
-      color: Theme.of(context).primaryColorLight,
-      child: Row(
-        children: <Widget>[
-          Text('@johnny_karate')
-        ],
-      ),
-    );
+    Container buildUserHeader({String username, String avatarUrl}) {
+      return Container(
+        padding: const EdgeInsets.all(8.0),
+        color: Theme.of(context).primaryColorLight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage(avatarUrl),
+              radius: 16.0,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text(
+                username,
+                style: TextStyle(fontWeight: FontWeight.bold)
+              )
+            )
+          ],
+        ),
+      );
+    }
 
-    Widget imageCaption = Container(
-      padding: EdgeInsets.all(8.0),
-      color: Theme.of(context).primaryColor,
-      child: Row(
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "@johnny_karate@pawnee.social"
-              ),
-              Text(
-                  "This is a rad caption. Cool beans!"
+    Container buildImageCaption({String username, String caption}) {
+      return Container(
+        padding: EdgeInsets.all(8.0),
+        color: Theme.of(context).primaryColor,
+        child: Container(
+            child: Wrap(
+              children: <Widget> [
+                Text(
+                  username,
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-            ],
+                Text(caption),
+              ]
+            ),
           )
-        ],
-      )
-    );
+      );
+    }
 
     return Column(
       children: <Widget>[
-        userHeader,
-        Image.asset(
-          'images/spacey_1.png',
+        buildUserHeader(
+          username: "@pizza_suplex",
+          avatarUrl: "https://pbs.twimg.com/profile_images/943615961193697283/_xPN6A93_400x400.jpg"
+        ),
+        Image.network(
+          'https://01.keybase.pub/screenshots/plsrt.png',
           fit: BoxFit.cover 
         ),
-        imageCaption
+        buildImageCaption(
+          username: "@pizza_suplex@polygon.zone",
+          caption: "@NintendoAmerica Wow. Can't believe it's been 10 eyars since you became an angel. You loved Toad. Please Retweet."
+        )
       ],
     );
   }
