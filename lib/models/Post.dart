7d6d5a68cbdf2fea90eaf17d2f22633edec17818
Connor Hudson <cohu3291@colorado.edu>
2018-07-08 10:14:34 -0400

    Add more fields to the Post model

--- lib/models/Post.dart
+++ lib/models/Post.dart
@@ -1,12 +1,27 @@
 import 'package:morii/models/Account.dart';
 class Post {
+  // TODO: add visibility enum
   final Account account;
   final String mediaUrls; // TODO: add support for multiple attachments
   final String mediaTypes; // TODO: add support for multiple attachments
   final String caption;
+  final DateTime createdAt;
+  final bool isFavourited;
+  final bool isReblogged;
+  final bool isMuted;
   final bool isSensitive;
 
-  Post({this.account, this.mediaUrls, this.mediaTypes, this.caption, this.isSensitive});
+  Post({
+    this.account,
+    this.mediaUrls,
+    this.mediaTypes,
+    this.caption,
+    this.createdAt,
+    this.isSensitive,
+    this.isFavourited,
+    this.isReblogged,
+    this.isMuted
+  });
 
   factory Post.fromJson(Map<String, dynamic> json) { // This takes a full Mastodon/PixelFed Status entity
     if (json['account']['username'] == null) {
@@ -17,8 +32,13 @@ class Post {
       caption: json['content'],
       mediaUrls: json['media_attachments'][0]['url'], // TODO: Change this to support multiple media urls
       mediaTypes: json['media_attachments'][0]['type'], // TODO: Change this to support multiple media urls
+      createdAt: DateTime.parse(json['created_at']),
       account: Account.fromJson(json['account']), // TODO: should this just be Account(json['account'])?
-      isSensitive: json['sensitive']
+      isSensitive: json['sensitive'],
+      isFavourited: json['favourited'],
+      isReblogged: json['reblogged'],
+      isMuted: json['muted'],
+      // TODO: Add Mentions and Tags to Post model
     );
   }
 
