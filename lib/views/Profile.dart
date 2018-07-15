2efb566a72fefacf694ab59882098232c5f307d3
Connor Hudson <cohu3291@colorado.edu>
2018-07-15 18:28:36 -0400

    Add Profile view stub

---
+++ lib/views/Profile.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:morii/models/Post.dart';
+import 'package:morii/models/Account.dart';
+
+import 'package:morii/components/Timeline.dart';
+import 'package:morii/components/BottomNav.dart';
+
+final String accessToken = "ACCESS_TOKEN_HERE"; // TODO: Put this into an OAath flow
+
+// TODO: Fetch account, posts
+
+class Profile extends StatefulWidget {
+  Profile({
+      Key key,
+      @required this.account
+    }) : super(key: key);
+
+  final Account account;
+
+  @override
+  createState() => new _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColorDark,
+      body: Center(
+        child: Placeholder()
+      ),
+      bottomNavigationBar: BottomNav(),
+      floatingActionButtonLocation: 
+        FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: new FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'New Post',
+        child: new Icon(Icons.camera_alt),
+        notchMargin: 4.0,
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
