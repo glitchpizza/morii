391ffc1ead68fc385870414891959625d90bf9b3
Connor Hudson <cohu3291@colorado.edu>
2018-08-14 16:41:37 -0600

    Move themes to their own file

---
+++ lib/util/Themes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = new ThemeData(
+      primaryColor: Colors.grey[50],
+      primaryColorLight: Colors.white,
+      primaryColorDark: Colors.grey[200],
+      accentColor: Colors.black,
+      brightness: Brightness.light,
+      iconTheme: new IconThemeData(
+        color: Colors.blueGrey[900]
+      ),
+      fontFamily: 'Source Sans Pro'
+    );
+
+final ThemeData darkTheme = new ThemeData(
+      primaryColor: Colors.blueGrey[800],
+      primaryColorLight: Colors.blueGrey[700],
+      primaryColorDark: Colors.blueGrey[900],
+      accentColor: Colors.white,
+      brightness: Brightness.dark,
+      iconTheme: new IconThemeData(
+        color: Colors.white
+      )
+    );
+
+  final ThemeData oledTheme = new ThemeData(
+      primaryColor: Colors.blueGrey[900],
+      primaryColorLight: Colors.black,
+      primaryColorDark: Colors.black,
+      accentColor: Colors.white,
+      brightness: Brightness.dark,
+      iconTheme: new IconThemeData(
+        color: Colors.white
+      )
+  );
