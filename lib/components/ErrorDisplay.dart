d386a158c0c7378fc645fdee05746f6e7e74dc03
Connor Hudson <cohu3291@colorado.edu>
2018-08-19 08:12:33 -0600

    Experiment with type-specific error handling

--- lib/components/ErrorDisplay.dart
+++ lib/components/ErrorDisplay.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class ErrorDisplay extends StatelessWidget {
-  final Object error;
+class ErrorDisplay<T> extends StatelessWidget {
+  final T error;
   final List<String> quips = [
     "oops.",
     "aw beans.",
@@ -10,6 +12,31 @@ class ErrorDisplay extends StatelessWidget {
 
   ErrorDisplay({this.error});
 
+  List<Widget> buildErrorMessage(T error) {
+    if (error is SocketException) {
+        SocketException e = error as SocketException;
+        String resolution;
+        switch (e.osError.errorCode) {
+          case 7:
+            resolution = 'Check your network connection.';
+        }
+        return [
+          Text('A network error occurred. (error ${e.osError.errorCode})', style: TextStyle(
+            fontSize: 20.0
+          )),
+          Text(resolution, style: TextStyle(
+            fontSize: 20.0
+          ))
+        ];
+    }
+
+    return [
+      Text('An unknown error has occurred.', style: TextStyle(
+        fontSize: 20.0
+      ))
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Center(
@@ -17,23 +44,17 @@ class ErrorDisplay extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: Text((quips..shuffle()).first,
-                style: TextStyle(
-                  fontFamily: 'Rachel',
-                  fontSize: 60.0, // TODO: Auto-sizing this based on text width?
-                  color: Color(0xFF0077FF),
-                ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20.0),
+            child: Text((quips..shuffle()).first,
+              style: TextStyle(
+                fontFamily: 'Rachel',
+                fontSize: 60.0, // TODO: Auto-sizing this based on text width?
+                color: Color(0xFF0077FF),
               ),
             ),
-          Text('A network error occurred.', style: TextStyle(
-            fontSize: 20.0
-          )),
-          Text('Please try again in a little while.', style: TextStyle(
-            fontSize: 20.0
-          ))
-        ],
+          ),
+        ]..addAll(buildErrorMessage(error)),
       )
     );
   }
